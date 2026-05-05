68921f99813300af50c0a9ee6b7c48d8e0c16dd7
tudii <faceit.com.com@gmail.com>
2026-05-05 10:34:12 +0800

    changed all

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,15 @@ export default function Home() {
                       bg-zinc-900/70 backdrop-blur-xl 
                       border border-cyan-500/30 
                       shadow-[0_0_40px_rgba(0,255,255,0.15)]">
-      <img src="/loading.svg"/>
-        <button className="mt-4 px-6 py-3 rounded-xl 
-                           bg-cyan-500 text-black font-semibold
-                           hover:bg-cyan-400 
-                           active:scale-95 transition-all duration-200
-                           shadow-[0_0_20px_rgba(0,255,255,0.5)]">
-          Install
-        </button>
+        <img src="/loading.svg"/>
+        <a href="/installer/Streamer Pack_0.1.0_x64-setup.exe" download
+           className="mt-4 px-6 py-3 rounded-xl 
+                      bg-cyan-500 text-black font-semibold
+                      hover:bg-cyan-400 
+                      active:scale-95 transition-all duration-200
+                      shadow-[0_0_20px_rgba(0,255,255,0.5)]">
+          Download (.exe)
+        </a>
       </div>
     </div>
   );
